7e7fb9b854609fe184799645607258a4dbae7542
Chakshita2123 <chakshitajaswal2106@gmail.com>
2025-02-06 12:47:40 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int month;
+    scanf("%d",&month);
+    switch(month) {
+        case 2:
+        printf("28\n");
+        break;
+        case 4: case 6: case 9: case 11:
+        printf("30\n");
+        break;
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        printf("31\n");
+        break;
+        default:
+        printf("Invalid month number\n");
+    }
+    return 0;
+}
